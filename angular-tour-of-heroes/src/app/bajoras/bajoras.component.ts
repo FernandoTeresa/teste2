384dc8dce432ec5957820529ee7f6cb823d71e96
FernandoTeresa <fernando_silvat@hotmail.com>
2022-09-30 11:36:43 +0100

    New commit

---
+++ angular-tour-of-heroes/src/app/bajoras/bajoras.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bajoras',
+  templateUrl: './bajoras.component.html',
+  styleUrls: ['./bajoras.component.css']
+})
+export class BajorasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
